b633bdfbcd6c9bcfe219e1454e197b473bf6975e
Brian <leehongjie91@gmail.com>
2016-05-17 15:38:32 +0800

    transaction order bug patch

---
+++ resources/views/email/marketing_registration.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>Please Login to the following URL based on the credentials given:</p>
+        <p><span style="font-weight:bold">URL:</span>&nbsp;{{ $url }}</p>
+        <p><span style="font-weight:bold">Username:</span>&nbsp;<strong>{{ $username }}</strong></p>
+        <p><span style="font-weight:bold">Password:</span>&nbsp;<strong>{{ $password }}</strong></p>
+        <p>Kindly change the password once you login.</p>
+    </body>
+</html>
