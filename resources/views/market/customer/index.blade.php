7b0ab9da744520cd2f17ef110bb2656f6e2adc1a
Brian <leehongjie91@gmail.com>
2016-06-11 01:53:33 +0800

    big patch mlm

--- resources/views/market/customer/index.blade.php
+++ resources/views/market/customer/index.blade.php
@@ -152,7 +152,7 @@ Customers
                                 <td class="col-md-1 text-center">@{{ customer.active }}</td>
                             </tr>
                             <tr ng-if="(customers | filter:search).length == 0 || ! customers.length">
-                                <td colspan="9" class="text-center">No Records Found</td>
+                                <td colspan="10" class="text-center">No Records Found</td>
                             </tr>
 
                         </tbody>
