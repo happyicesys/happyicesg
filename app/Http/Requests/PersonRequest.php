b2f635d499b0f2eb9b736a78ca8b484735c29c0a
Brian <leehongjie91@gmail.com>
2016-10-25 17:56:23 +0800

    person request disable id name (company) unique

--- app/Http/Requests/PersonRequest.php
+++ app/Http/Requests/PersonRequest.php
@@ -27,7 +27,6 @@ class PersonRequest extends Request
 
         return [
             'cust_id' => 'required|unique:people,cust_id,'.$person,
-            'company' => 'unique:people,company,'.$person,
             'name'=>'min:3',
             'roc_no' => 'unique:people,roc_no,'.$person,
             'email'=>'email|unique:people,email,'.$person,
@@ -35,6 +34,22 @@ class PersonRequest extends Request
             'alt_contact'=>array('regex:/^([0-9\s\-\+\(\)]*)$/'),
             'postcode' => 'numeric',
             'cost_rate' => 'integer',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'cust_id.required' => 'Please fill in the ID',
+            'cust_id.unique' => 'The ID has been taken',
+            'name.min' => 'Attn To must more than 3 words',
+            'roc_no.unique' => 'The ROC No has been taken',
+            'email.unique' => 'The Email has been taken',
+            'email.email' => 'The email format is incorrect',
+            'contact.regex' => 'The contact number only accepts 0-9, +, -',
+            'alt_contact.regex' => 'The Alt contact number only accepts 0-9, +, -',
+            'postcode.numeric' => 'The postcode must be in numbers',
+            'cost_rate.integer' => 'Cost rate need to be in whole number',
         ];
     }
 }
