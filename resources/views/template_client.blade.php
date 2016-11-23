c79fe129e3b4e9e25c6d055318a8af9eeedbc126
Brian <leehongjie91@gmail.com>
2016-11-23 11:17:10 +0800

    alert alignment

--- resources/views/template_client.blade.php
+++ resources/views/template_client.blade.php
@@ -6,13 +6,16 @@
 
     <body id="page-top">
         @include('partials.nav_client')
-            <div style="margin: 50px 0px 0px 0px;">
+            <div style="padding-top: 50px">
                 @include('errors.validate')
                 @include('flash::message')
             </div>
                 @yield('content')
             @include('partials.footer_client')
         @yield('footer')
+        <script>
+            $('div.alert').not('.alert-important').delay(6000).fadeOut(350);
+        </script>
     </body>
 </html>
 
