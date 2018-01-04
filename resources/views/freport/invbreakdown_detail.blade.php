59a5ebca4105e8a8bd06abf8e33d2904731dc89e
Brian <brian@BrianMac.local>
2018-01-04 13:39:45 +0800

    enable view deactivate customer freport

--- resources/views/freport/invbreakdown_detail.blade.php
+++ resources/views/freport/invbreakdown_detail.blade.php
@@ -20,7 +20,6 @@
                     {!! Form::label('person_id', 'Customer', ['class'=>'control-label search-title']) !!}
                     {!! Form::select('person_id', [''=>null]+
                         $people::select(DB::raw("CONCAT(cust_id,' - ',company) AS full, id"))
-                            ->whereActive('Yes')
                             ->where('cust_id', 'NOT LIKE', 'H%')
                             ->whereHas('profile', function($q) {
                                 $q->filterUserProfile();
