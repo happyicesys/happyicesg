28a8e1b956deab4d1b202df6fad9b3ee75fce56e
Brian <brian@BrianMac.local>
2019-05-07 10:24:34 +0800

    edit pdf

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltopdf',
+        'binary' => '/usr/local/bin/wkhtmltopdf-amd64',
         'timeout' => false,
         'options' => array(
                             'print-media-type' => true,
@@ -17,7 +17,7 @@ return array(
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltoimage',
+        'binary' => '/usr/local/bin/wkhtmltoimage-amd64',
         'timeout' => false,
         'options' => array(),
     ),
