3ecaf7cb831e378131b9ac03b63a10bc416b982d
Brian Lee Hong Jie <brian@Brians-MacBook-Air.local>
2019-12-23 15:15:16 +0800

    test next day delivery jotform

---
+++ resources/views/client/nextdaydelivery.blade.php
@@ -0,0 +1,8 @@
+@extends('template_client')
+@section('title')
+Healthier Life
+@stop
+@section('content')
+
+<script type="text/javascript" src="https://form.jotform.me/jsform/92795046850466"></script>
+@stop
