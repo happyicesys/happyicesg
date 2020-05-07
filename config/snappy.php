95efeddf0f11ce6e9fc8cae6ce5d2b7b13cb79d5
Brian Lee Hong Jie <brian@Brians-MacBook-Air.local>
2020-05-07 16:17:05 +0800

    pdf config

--- config/snappy.php
+++ config/snappy.php
@@ -7,12 +7,22 @@ return array(
         'enabled' => true,
         'binary' => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
+        'no-images' => false,
+        'encoding' => 'utf-8',
+        'disable-smart-shrinking' => true,
+        'margin-top' => 0,
+        'margin-left' => 0,
+        'margin-right' => 0,
+        'margin-bottom' => 0,
         'options' => array(
                             'print-media-type' => true,
                             'outline' => true,
                             // 'dpi' => 96,
                             'dpi' => 100,
                             'page-size' => 'A4',
+                            'enable-javascript' => true,
+                            'javascript-delay' => 3000,
+                            'enable-smart-shrinking' => true
                         ),
     ),
     'image' => array(
