cee5ee501e7597bb3177a65826d7bc0afb84aeaa
Brian Lee Hong Jie <brian@Brians-MacBook-Air.local>
2020-07-22 12:15:05 +0800

    debug mechandiser

--- app/Http/Controllers/OperationWorksheetController.php
+++ app/Http/Controllers/OperationWorksheetController.php
@@ -935,7 +935,7 @@ class OperationWorksheetController extends Controller
                 $items = clone $deals;
                 $qty = $qty->sum('deals.qty');
                 $total = $total->select(DB::raw('SUM(CASE WHEN transactions.gst=1 THEN(CASE WHEN transactions.is_gst_inclusive=0 THEN deals.amount*((100 + transactions.gst_rate)/100) ELSE deals.amount END) ELSE deals.amount END) AS total'))->get('total');
-                $items = $items->sum('deals.qty')->groupBy('items.id')->get();
+                $items = $items->select('items.product_id', DB::raw('SUM(deals.qty) AS qty'))->groupBy('items.id')->get();
 
                 $transactions =  DB::table('transactions')
                         ->where('transactions.person_id', $person->person_id)
