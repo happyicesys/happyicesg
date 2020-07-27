63597e783ce96e0b61ce5bc7285bc0155d8b7fd9
Brian Lee Hong Jie <brian@Brians-MacBook-Air.local>
2020-07-27 16:31:36 +0800

    debug merchandiser

--- app/Http/Controllers/OperationWorksheetController.php
+++ app/Http/Controllers/OperationWorksheetController.php
@@ -1048,9 +1048,9 @@ class OperationWorksheetController extends Controller
                                 END) ELSE x.total END) + (CASE WHEN x.delivery_fee>0 THEN x.delivery_fee ELSE 0 END)) AS total'),
                         DB::raw('ROUND(x.total_qty, 1) AS total_qty')
                     )
-                    ->groupBy('x.id');
-            dd($transaction->get());
-                    // ->first();
+                    ->groupBy('x.id')
+            // dd($transaction->get());
+                    ->first();
 
         $deals = $deals->select(
                     'items.product_id',
