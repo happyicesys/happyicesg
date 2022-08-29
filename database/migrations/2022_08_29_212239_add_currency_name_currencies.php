62614ce514b932043774067dd38aaac119351eeb
Brian Lee <brian@Brians-MBP.realtek>
2022-08-29 21:23:40 +0800

    currency name currencies

---
+++ database/migrations/2022_08_29_212239_add_currency_name_currencies.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCurrencyNameCurrencies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('currencies', function (Blueprint $table) {
+            $table->string('currency_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('currencies', function (Blueprint $table) {
+            $table->dropColumn('currency_name');
+        });
+    }
+}
