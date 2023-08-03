710ea0e004f7da52939d73d848c079f1c2256317
Brian Lee <brian@Brians-MacBook-Pro.local>
2023-08-03 16:10:35 +0800

    batch job assign merchandier

--- resources/views/transaction/jobassign.blade.php
+++ resources/views/transaction/jobassign.blade.php
@@ -140,7 +140,7 @@ Job Assign
                                 <option value="">All</option>
                                 <option value="-1">-- Unassigned --</option>
                                 @foreach($users::where('is_active', 1)->orderBy('name')->get() as $user)
-                                    @if(($user->hasRole('driver') or $user->hasRole('technician') or $user->hasRole('driver-supervisor') or $user->id === 100010) and count($user->profiles) > 0)
+                                    @if(($user->hasRole('driver') or $user->hasRole('technician') or $user->hasRole('driver-supervisor') or $user->id === 100010 or $user->hasRole('merchandiser')) and count($user->profiles) > 0)
                                         <option value="{{$user->name}}">
                                             {{$user->name}}
                                         </option>
